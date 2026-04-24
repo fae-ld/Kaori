2d5d9ef2581b5b9e5b54aff6058fe77d3d4a2be6
fae-ld <this.fairuz@gmail.com>
2026-04-24 14:56:47 +0700

    Create Request Response Schema

---
+++ app/schemas.py
@@ -0,0 +1,12 @@
+# app/schemas.py
+from pydantic import BaseModel
+from typing import List, Any, Dict
+
+class JournalRequest(BaseModel):
+    username: str
+    aliases: List[str]
+    entry_text: str
+
+class ExtractionResponse(BaseModel):
+    status: str
+    data: Dict[str, Any]
